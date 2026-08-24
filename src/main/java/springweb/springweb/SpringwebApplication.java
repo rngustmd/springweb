c38f8872db837e0dcc5aedb4fe286bd2132c1f76
rngustmd <hsk1485@gmail.com>
2026-08-24 14:38:39 +0900

    springboot start 0824

--- src/main/java/springweb/springweb/SpringwebApplication.java
+++ src/main/java/springweb/springweb/SpringwebApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringwebApplication {
 
+
+	// [1] 해당 클래스파일 ctrl+f5 실행 후 , 인터넷 켜고 localhost:8000 접속 후 Error page
 	public static void main(String[] args) {
 		SpringApplication.run(SpringwebApplication.class, args);
 	}
