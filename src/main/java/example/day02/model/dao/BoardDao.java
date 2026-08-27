d96705ad60444686b87f012179cf1f1d82f83648
rngustmd <hsk1485@gmail.com>
2026-08-27 12:24:08 +0900

    0827 3hr

--- src/main/java/example/day02/model/dao/BoardDao.java
+++ src/main/java/example/day02/model/dao/BoardDao.java
@@ -8,8 +8,7 @@ import java.util.ArrayList;
 import example.day02.model.dto.BoardDto;
 
 public class BoardDao extends BaseDao {
-
-    private BoardDao( ){ };
+    private BoardDao(){};
     private static final BoardDao instance = new BoardDao();
     public static BoardDao getInstance( ){ return instance; }
 
@@ -30,28 +29,24 @@ public class BoardDao extends BaseDao {
         return false; // 등록 실패시 false 
     }
 
-    // [2] 전체조회 DAO
-    public ArrayList<BoardDto> findAll( ){
-        ArrayList<BoardDto> list = new ArrayList<>(); // 2.7 레코드 정보 들을 담을 리스트
-        try{
-            String sql = "select * from board"; // 2.1 SQL 작성한다.
-            PreparedStatement ps = conn.prepareStatement( sql ); // 2.2 SQL 기재한다. *예외발생 
-            // 2.3 ?매개변수 대입한다.<생략>
-            ResultSet rs =  ps.executeQuery(); // 2.4 기재된 SQL 실행 , .executeQuery() select
-            // 2.5 SQL 결과( select 조회 결과는 항상 테이블로 반환한다. ) 즉] 레코드 하나씩 타입변환
-            while( rs.next() ){ // rs.next() : 다음 레코드(행) 이동 , 마지막 레코드까지 하나씩 이동 반복 뜻 // 레코드 수만큼 반복
-                // 2.6 현재 레코드의 필드값 들을 --> DTO 변환
+    // [2] 전체조회
+    public ArrayList< BoardDto > findAll( ){
+        ArrayList<BoardDto> list = new ArrayList<>(); // 레코드1개당 DTO1개 , DTO여러개 -> 배열/리스트
+        try{String sql = "select * from board" ;// 1. SQL 작성
+            PreparedStatement ps = conn.prepareStatement( sql ); // 2. SQL 기재
+            // 3. 기재된 SQL 매개변수 대입 ( SQL내 ? 없어서 생략 )
+            ResultSet rs = ps.executeQuery(); // 4. SQL 실행 , insert/update/delete => executeUpdate() , select => executeQuery()
+            // 5. SQL 결과 처리
+            while( rs.next() ){ // 다음 레코드 뜻 
                 BoardDto boardDto = new BoardDto(); 
-                boardDto.setNo( rs.getInt("no") ); // rs.get타입( "가져올속성명" )
-                boardDto.setContent( rs.getString("content") );
-                boardDto.setWriter( rs.getString("writer") );
-                // 2.7 변환한 DTO --> 리스트에 담기
-                list.add( boardDto );
+                boardDto.setNo( rs.getInt("no") ); // rs.get타입( 가져올속성명 );
+                boardDto.setContent( rs.getString("content"));
+                boardDto.setWriter( rs.getString( "writer") );
+                list.add(boardDto);
             }
-        }catch( SQLException e ){ System.out.println(e); } 
-        // 2.8 리스트 반환
-        return list; 
-    } // 전체조회 end 
+        }catch(Exception e ){ System.out.println( e ); }
+        return list;
+    }
 
     // [3] 개별수정 DAO
     public boolean update( BoardDto boardDto ){
@@ -76,7 +71,5 @@ public class BoardDao extends BaseDao {
         }catch( SQLException e ){ System.out.println( e ); }
         return false;
     }
-    
 
-}
-
+} // class end 
