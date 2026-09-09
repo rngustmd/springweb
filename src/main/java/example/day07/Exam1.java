71ca932f8fecaff469469fef440a175f63e2f061
rngustmd <hsk1485@gmail.com>
2026-09-09 13:20:26 +0900

    0909 4hr

---
+++ src/main/java/example/day07/Exam1.java
@@ -0,0 +1,85 @@
+package example.day07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Exam1 {
+    public static void main(String[] args) {
+
+        // 서로 다른 클래스의 메소드 호출하는 방법
+        // 메소드란? 상호작용( 2개 이상의 개체 주고(인수M)받는(리턴1) ) 
+        // 1. 인스턴스 생성하여 메소드 호출
+        TestService testService = new TestService();
+        int result1 = testService.plus(3, 5);
+
+        // 2. 싱글톤
+        // TestService testService = TestService.getInstance();
+        // int result2 = testService.plus(3, 5);
+
+        // 3. 메소드가 static이면
+        int result3 = TestService.plus2( 10 , 5);
+
+        // 4. 스프링방식:
+        // @Component(자동인스턴스생성)@Autowired(인스턴스호출)
+        // @Autowired private TestService testService;
+        // int result4 = testService.plus(10, 5);
+        
+        // 인스턴스(주체p) VS static(주체x)
+        TestService t1 = new TestService();
+        t1.달리기();
+
+        // TestService t2 = new TestService();
+        // t2.달리기2();
+        // TestService.달리기2();
+        // memberDto.toEntity() <---- 현재 dto 인스턴스가
+        // MemberDto.from( entity ); < -- 변환할 엔티티
+
+        // 5. 사칙연산( 연산은 하나의 값 반환 )
+        int x = 10 + 2 + 5; // 10 + 2 => 12 + 5 => 17
+
+        TestService t3 = new TestService();
+        t3.개별호출().밥먹기();
+
+
+        
+    }
+
+}
+
+class TestService{
+
+    // private TestService(){}
+    // private static final TestService instance = new TestService();
+    // public static TestService getInstance(){ return instance; }
+
+    int plus( int x , int y ){
+        return x + y;
+    }
+    static int plus2( int x , int y ){ return x + y;}
+
+    void 달리기(){
+        System.out.println( this );
+    }
+    // static void 달리기2(){
+    //     System.out.println( this );
+    // }
+
+    List<Student> list = new ArrayList<>();
+    TestService( ){
+        list.add( new Student("강호동") );
+        list.add( new Student("신동엽") );
+    }
+    Student 개별호출( ){ return list.get(1) }
+
+}
+
+class Student{
+    String name;
+    void 밥먹기(){
+        System.out.println( this.name + "밥 먹는다.");
+    }
+    Student( String name ){this.name = name;}
+
+}
---
+++ src/main/java/example/day07/Exam4.java
@@ -0,0 +1,83 @@
+package example.day07;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class Exam4 {
+    public static void main(String[] args) {
+    // 메소드 레퍼런스 : 람다식이 단순히 기존메소드만 호출하는 경우 간결하게 표현하는 방법
+    // 1. 
+    // Integer.parseInt("문자"); // 문자 --> 정수 변환 함수
+    Function< String , Integer > function = ( x ) -> { return Integer.parseInt(x); };
+    Function< String , Integer > function2 = Integer::parseInt;
+        System.out.println( function2.apply("10") ); // "10" -> 10
+    
+    // 2.
+    List< String > names = List.of( "유재석","강호동","신동엽","서장훈" );
+        // 1)
+        for( int index = 0 ; index <= names.size() ; index ++ ){
+            System.out.println( names.get(index) );
+        }
+        // 2)
+        for( String name : names ){ 
+            System.out.println( name );
+        }
+        // 3)
+        names.stream().forEach((name) -> {
+            System.out.println(name);
+        });
+        // 4) 메소드참조는 메소드명 명시하고 ()소괄호 작성하지 않는다.
+        names.stream().forEach( System.out::println );
+
+    // 3. 이름들의 글자 수 출력하세요
+        // 전통 방식
+        for( int index = 0 ; index <= names.size() ; index ++ ){
+            System.out.println( names.get(index).length() ); // 글자수
+        } 
+        // 스트림 방식
+        names.stream().map((name) -> { return name.length(); }).forEach((result) -> { System.out.println( result ); });
+        // 메소드 레퍼런스(참조) 방식 , 주의할점은 메소드 뒤에 () 소괄호 사용하지 않는다. 내부적으로 메소드 호출하는 구조
+        names.stream().map( String::length ).forEach( System.out::println );
+    
+    // 4. names 리스트내 문자열/이름들을 각각 대입하여 Student 객체 만드세요
+        // 전통방식
+        List<Student> list1 = new ArrayList<>();
+        for( int index = 0 ; index <= names.size( ) -1 ; index ++ ){
+            Student student = new Student( names.get( index ) );
+            list1.add( student );}
+
+        // 스트림API
+        List<Student> list2 = names.stream( ).map( ( name ) -> { return new Student(name); }).toList( );
+
+        // 메소드참조(레퍼런스)
+        List<Student> list3 = names.stream( ).map( Student::new ).toList( );
+
+        /*
+        
+            유형
+            1. 클래스명::static메소드명
+            2. 인스턴스명::메소드명
+            3. 클래스명::new
+
+            JPA 서비스 구조 : entity --> dto 변환
+            // 1.
+            List<MemberDto> list = entityList.stream().map( ( entity ) -> { return MemberDto.from( entity ); } ).toList();
+            // 2.
+            List<MemberDto> list = entityList.stream().map( MemberDto::from ).toList();
+
+            return boardRepository.findAll().stream().map( (entity)->{ return BoardDto.from(entity); }  ).toList();
+            return boardRepository.findAll().stream().map( BoardDto::from  ).toList();
+
+        
+        */
+
+    } // MAIN END
+
+} // CLASS END
+
+class Student{
+    private String name;
+    public Student( String name ){ this.name = name; }
+}
+
